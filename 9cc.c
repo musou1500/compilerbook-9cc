bb7454a138c5b2993478abb96b38f5588ab5cacc
musou1500 <musou1500@gmail.com>
2018-11-04 18:34:41 +0900

    トークナイザを導入

--- 9cc.c
+++ 9cc.c
@@ -1,5 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+enum {
+  TK_NUM = 256,
+  TK_EOF,
+};
+
+typedef struct {
+  int ty;
+  int val;
+  char *input;
+} Token;
+
+Token tokens[100];
+
+void tokenize(char *p) {
+  int i = 0;
+  while (*p) {
+    if (isspace(*p)) {
+      p++;
+      continue;
+    }
+
+    if (*p == '+' || *p == '-') {
+      tokens[i].ty = *p;
+      tokens[i].input = p;
+      i++;
+      p++;
+      continue;
+    }
+
+    if (isdigit(*p)) {
+      tokens[i].ty = TK_NUM;
+      tokens[i].input = p;
+      tokens[i].val = strtol(p, &p, 10);
+      i++;
+      continue;
+    }
+
+    fprintf(stderr, "トークないずできません: %s\n", p);
+    exit(1);
+  }
+
+  tokens[i].ty = TK_EOF;
+  tokens[i].input = p;
+}
+
+void error(int i) {
+  fprintf(stderr, "予期せぬトークンです: %s\n", tokens[i].input);
+  exit(1);
+}
+
 
 int main(int argc, char **argv)
 {
@@ -8,27 +61,37 @@ int main(int argc, char **argv)
     return 1;
   }
 
-  char *p = argv[1];
+  tokenize(argv[1]);
 
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
-  printf("  mov rax, %ld\n", strtol(p, &p, 10));
-  while (*p) {
-    if (*p == '+') {
-      p++;
-      printf("  add rax, %ld\n", strtol(p, &p, 10));
+  
+
+  if (tokens[0].ty != TK_NUM)
+    error(0);
+  printf("  mov rax, %d\n", tokens[0].val);
+  int i = 1;
+  while (tokens[i].ty != TK_EOF) {
+    if (tokens[i].ty == '+') {
+      i++;
+      if (tokens[i].ty != TK_NUM)
+        error(i);
+      printf("  add rax, %d\n", tokens[i].val);
+      i++;
       continue;
     }
 
-    if (*p == '-') {
-      p++;
-      printf("  sub rax, %ld\n", strtol(p, &p, 10));
+    if (tokens[i].ty == '-') {
+      i++;
+      if (tokens[i].ty != TK_NUM)
+        error(i);
+      printf("  sub rax, %d\n", tokens[i].val);
+      i++;
       continue;
     }
-
-    fprintf(stderr, "予期せぬ文字です: '%c'\n", *p);
-    return 1;
+  
+    error(i);
   }
   
   printf("  ret\n");
