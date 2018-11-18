6d062427f1f666142d0a913ec887b0baaea32274
musou1500 <musou1500@gmail.com>
2018-11-19 04:40:09 +0900

    構造体を使った関数を lib.c に追加

--- rt/lib.c
+++ rt/lib.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "./lib.h"
 
 int add_int(int x, int y) {
   return x + y;
+}
+
+Data *new_data(int x, int y) {
+  Data* d = malloc(sizeof(Data));
+  d->x = x;
+  d->y = y;
+  return d;
+}
+
+int sum_data(Data* d) {
+  return d->x + d->y;
 }
--- rt/lib.h
+++ rt/lib.h
@@ -1,1 +1,8 @@
+typedef struct {
+  int x;
+  int y;
+} Data;
+
 extern int add_int(int x, int y);
+extern Data *new_data(int x, int y);
+extern int sum_data(Data* d);
